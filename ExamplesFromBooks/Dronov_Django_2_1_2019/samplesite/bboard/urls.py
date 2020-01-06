733e7d4f986c0dfd40abdd6edfb8516bb8ab6612
Владимир Ященко <47042283+vsyaschenko@users.noreply.github.com>
2020-01-06 12:54:23 +0300

    last page: 62

--- ExamplesFromBooks/Dronov_Django_2_1_2019/samplesite/bboard/urls.py
+++ ExamplesFromBooks/Dronov_Django_2_1_2019/samplesite/bboard/urls.py
@@ -4,6 +4,6 @@ from .views import index, by_rubric
 
 #Ссылки
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index)#Главная вызывает метод index
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index')#Главная вызывает метод index
 ]
