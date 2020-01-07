c443f066b13dee68737c3849ec40a10e57a4f9f2
Владимир Ященко <47042283+vsyaschenko@users.noreply.github.com>
2020-01-07 16:45:29 +0300

    Dronov: page 68

--- Dronov_Django_2_1_2019/samplesite/bboard/urls.py
+++ Dronov_Django_2_1_2019/samplesite/bboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 #Ссылки
 urlpatterns = [
+    #аргумент name содержит имя используемое в шаблонах для тега url
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('', index, name='index')#Главная вызывает метод index
+    path('', index, name='index'),#Главная вызывает метод index
+    path('add/', BbCreateView.as_view(), name='add')
 ]
--- Dronov_Django_2_1_2019/samplesite/bboard/views.py
+++ Dronov_Django_2_1_2019/samplesite/bboard/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bb, Rubric
+
+from .forms import BbForm
 
 def index(request):
     # #Загружаем шаблон
@@ -27,3 +31,13 @@ def by_rubric(request, rubric_id):
                 'rubrics': rubrics,
                 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
